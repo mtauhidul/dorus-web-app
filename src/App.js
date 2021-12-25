3c52ba59e3bcc36fe2008aea2d02e7f18982e0b7
mtauhidul <mislam.tauhidul@gmail.com>
2021-12-25 10:04:19 +0600

    update branch

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Crm from "./views/Crm";
-import Home from "./views/Home";
-import MobileApp from "./views/MobileApp";
-import OpenApi from "./views/OpenApi";
-import Wealth from "./views/Wealth";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Crm from './views/Crm';
+import Home from './views/Home';
+import MobileApp from './views/MobileApp';
+import OpenApi from './views/OpenApi';
+import Wealth from './views/Wealth';
 
 function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/mobile-app">
-					<MobileApp />
-				</Route>
-				<Route path="/crm">
-					<Crm />
-				</Route>{" "}
-				<Route path="/wealth">
-					<Wealth />
-				</Route>
-				<Route path="/open-api">
-					<OpenApi />
-				</Route>
-			</Switch>
-		</Router>
-	);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/mobile-app'>
+          <MobileApp />
+        </Route>
+        <Route path='/crm'>
+          <Crm />
+        </Route>{' '}
+        <Route path='/wealth'>
+          <Wealth />
+        </Route>
+        <Route path='/open-api'>
+          <OpenApi />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
